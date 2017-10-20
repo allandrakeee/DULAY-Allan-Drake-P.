d0e68d190402d8e55185c763f09bfbd14125f284
allandrakeee <allandulay69@gmail.com>
2017-10-20 12:22:01 -0800

    Added to source code.

--- #6/main.py
+++ #6/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hello YouTube'
+	return 'This is a test'
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=8080, debug=True)
